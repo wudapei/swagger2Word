7cd8a2859305caa49075da13dbecffee76c6b06e
wudapei <53213006+wudapei@users.noreply.github.com>
2023-01-06 17:02:55 +0800

    Add files via upload

---
+++ swagger2word/src/main/java/org/word/controller/IndexController.java
@@ -0,0 +1,22 @@
+package org.word.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author xiuyin.cui
+ * @Description
+ * @date 2019/3/22 10:52
+ */
+@Controller
+public class IndexController {
+    @ApiIgnore
+    @RequestMapping(value = "/")
+    public String index(HttpServletRequest request) {
+        return "redirect:swagger-ui.html";
+    }
+}
